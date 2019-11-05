208c0b4200bd6221412c3880460ffb89f71f3686
fneary <fergus.neary@aig.com>
2019-11-05 16:20:41 +0000

    Solution

--- Day3IntroToConditionalStatements.java
+++ Day3IntroToConditionalStatements.java
@@ -5,26 +5,27 @@ public class Day3IntroToConditionalStatements {
 	public int randomIntegerBetween1And100() {
 		Random rand  = new Random();
 		//Generate a random number between 0-99
-		int randomInteger = rand.nextInt(99);
+		int randomInteger = rand.nextInt(19);
 		// In order to generate a random number between 1 and 100, we need to add 1		
 		randomInteger++;
 		return randomInteger;
 	}
 	
-	public String checkIntWeirdOrNot(int n) {
-		 if(n%2==0){
-	         if(n>=2 && n<=5)
-	             return "Not Weird";
-	          else if(n>=6 && n<=20)
-	        	  return "Weird";
-	           else
-	        	   return "Not Weird";
-	              
-	      }
-	      else{
-	        return "Weird";
-	      }
+	public String checkIntWeirdOrNot(int rand) {
+		System.out.println("The number is: " + rand);
+		
+		if (rand%2 != 0) {
+				return "Weird";	
+		} else if(rand%2 == 0 && 2<=rand && rand<=5) {
+			return "Not Weird";
+		} else if (rand%2 == 0 && 6<=rand && rand<=20) {
+			return "Weird";
+		} else if (rand%2 == 0 && rand >20){
+			return "Not Weird";
+		}	
+		return null;
 	}
+	
 	
 	public Day3IntroToConditionalStatements() {
 	}
@@ -33,6 +34,6 @@ public class Day3IntroToConditionalStatements {
 		// TODO Auto-generated method stub
 		Day3IntroToConditionalStatements weirdOrNot = new Day3IntroToConditionalStatements();
 		int x = weirdOrNot.randomIntegerBetween1And100();
-		System.out.print(weirdOrNot.checkIntWeirdOrNot(x));
+		System.out.println(weirdOrNot.checkIntWeirdOrNot(x));
 	}
-}	
+}
